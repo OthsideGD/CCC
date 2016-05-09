251c38e1dc9e33462a21838eae85b9725c9582a4
John D. Tess <shamsh1r@yandex.ru>
2016-05-09 16:10:14 +0300

    CreateClassSave

--- CraftCostCalc/Form1.cs
+++ CraftCostCalc/Form1.cs
@@ -21,29 +21,6 @@ namespace CraftCostCalc
         }
 
         //Класс для сохранения переменных
-        public class SaveSettings
-        {
-            //Простые ресурсы
-            public double SaveAdamantiteNugget, SaveAnimalBone, SaveAnimalSkin, SaveAsofe, SaveCharcoal, SaveCoal,
-            SaveEnria, SaveIronOre, SaveMithrilOre, SaveMoldGlue, SaveMoldHardener, SaveMoldLubricant,
-            SaveOriharukonOre, SaveSilverNugget, SaveStem, SaveStoneOfPurity, SaveSuede, SaveThons, SaveThread,
-            SaveVarnish, SaveCry_d, SaveCry_c, SaveCry_b, SaveCry_a, SaveCry_s, SaveGem_d, SaveGem_c, SaveGem_b, SaveGem_a, SaveGem_s;
-
-            //Крафтовые ресурсы
-            public double SaveBraidedHemp, SaveCoarsedBonePowder, SaveCokes, SaveCompoundBraid, SaveHighGradeSuede, SaveLeather, SaveMetalHardener, SaveMetallicThread, SaveDurableMetalPlate, SaveSteel, SaveCord, SaveMetallicFiber,
-            SaveCraftedLeather, SaveSteelMold, SaveSynteticCokes, SaveOriharukon, SaveVarnishOfPurity, SaveMithrilAlloy, SaveSilverMold, SaveBlacksmithsFrame, SaveArtisansFrame, SaveCraftsmanMold,
-            SaveArcsmithsAnvil, SaveMaestroAnvilLock, SaveLeolinsMold, SaveMaestroHolder, SaveMaestroMold, SaveWarsmithsHolder, SaveWarsmithsMold;
-
-            //Револ
-            public double SaveSwordOfRevolution;
-
-            //АА
-            public double SaveAncientAdena;
-
-            //Соул и спирит оре
-            public double SaveSoulOre, SaveSpiritOre;
-
-        }
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
---
+++ CraftCostCalc/SaveSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftCostCalc
+{
+    public class SaveSettings
+    {
+        //Простые ресурсы
+        public double SaveAdamantiteNugget, SaveAnimalBone, SaveAnimalSkin, SaveAsofe, SaveCharcoal, SaveCoal,
+        SaveEnria, SaveIronOre, SaveMithrilOre, SaveMoldGlue, SaveMoldHardener, SaveMoldLubricant,
+        SaveOriharukonOre, SaveSilverNugget, SaveStem, SaveStoneOfPurity, SaveSuede, SaveThons, SaveThread,
+        SaveVarnish, SaveCry_d, SaveCry_c, SaveCry_b, SaveCry_a, SaveCry_s, SaveGem_d, SaveGem_c, SaveGem_b, SaveGem_a, SaveGem_s;
+
+        //Крафтовые ресурсы
+        public double SaveBraidedHemp, SaveCoarsedBonePowder, SaveCokes, SaveCompoundBraid, SaveHighGradeSuede, SaveLeather, SaveMetalHardener, SaveMetallicThread, SaveDurableMetalPlate, SaveSteel, SaveCord, SaveMetallicFiber,
+        SaveCraftedLeather, SaveSteelMold, SaveSynteticCokes, SaveOriharukon, SaveVarnishOfPurity, SaveMithrilAlloy, SaveSilverMold, SaveBlacksmithsFrame, SaveArtisansFrame, SaveCraftsmanMold,
+        SaveArcsmithsAnvil, SaveMaestroAnvilLock, SaveLeolinsMold, SaveMaestroHolder, SaveMaestroMold, SaveWarsmithsHolder, SaveWarsmithsMold;
+
+        //Револ
+        public double SaveSwordOfRevolution;
+
+        //АА
+        public double SaveAncientAdena;
+
+        //Соул и спирит оре
+        public double SaveSoulOre, SaveSpiritOre;
+    }
+}
